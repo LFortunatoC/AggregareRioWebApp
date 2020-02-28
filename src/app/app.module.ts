e28b6ea82927a5381178378238e4b167d74bfcc1
julianaTamiazi <biju.juli@gmail.com>
2020-02-28 14:51:03 -0500

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
