aeebdbf524f1cd6e9355193f9daf1efa9706dc3d
Mauricio Dias <35377601+MauAFDias@users.noreply.github.com>
2020-03-03 02:55:56 -0500

    removed a test module, next start to make the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 import {FooterButtonsModule} from './footer-buttons/footer-buttons.module';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {FooterButtonsModule} from './footer-buttons/footer-buttons.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FooterButtonsModule
+    FooterButtonsModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
